049214cba82d8d7942cf2a1f0e9c766bc4a1a100
jcarrera94 <juankarrera@gmail.com>
2019-10-17 19:12:47 +0000

    minor changes

--- 1_queries/05_queries_allMyReservations.sql
+++ 1_queries/05_queries_allMyReservations.sql
@@ -1,9 +1,8 @@
 SELECT properties.id as id, properties.title as title, properties.cost_per_night as cost_per_night, reservations.start_date as start_date, avg(property_reviews.rating) as average_rating
 FROM reservations
-JOIN users ON users.id = reservations.guest_id
 JOIN properties ON properties.id = reservations.property_id
 JOIN property_reviews ON property_reviews.property_id = properties.id
-WHERE users.id = 1
+WHERE reservations.guest_id = 1
 GROUP BY properties.id, properties.title, properties.cost_per_night, reservations.start_date, reservations.end_date
 HAVING reservations.end_date < now()::date
 ORDER BY reservations.start_date
